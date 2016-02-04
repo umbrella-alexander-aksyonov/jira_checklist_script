2d6cdfa8130040b6d050d380011ac7e877473968
Anton Karmazin <anton.karmazin@umbrella-web.com>
2016-02-04 21:22:38 +0300

    Small fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var success = function(data) {
 	var html = data.fields[3].editHtml;
 	var dom = $.parseHTML(html);
 	var des = $('textarea[name=description]', dom).html();
-	var check = ( /{panel:.*}[\s\S]([\s\S]*)[\s\S]{panel}/i.exec(des) )[1];
+	var check = ( /{panel.*}[\s\S]([\s\S]*)[\s\S]{panel}/i.exec(des) )[1];
 	var list = check.replace('\r','').split('\n');
 
 
@@ -25,7 +25,7 @@ var success = function(data) {
 
 	var list2 = list.join("\r\n");
 
-	var des2 = des.replace(/({panel:.*}[\s\S])([\s\S]*)([\s\S]{panel})/i, "$1" + list2 + "$3");
+	var des2 = des.replace(/({panel.*}[\s\S])([\s\S]*)([\s\S]{panel})/i, "$1" + list2 + "$3");
 
 	var token = $("input[name=atl_token]").val();
 
