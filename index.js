764e0c42b324cf0e67bc46564605b5d8482b44aa
alexander.aksyonov <alexander.aksyonov@umbrella-web.com>
2016-02-05 11:10:32 +0300

    Refactoring, use issueId

--- index.js
+++ index.js
@@ -1,58 +1,79 @@
-
-var index;
-
-var success = function(data) {
-	var html = data.fields[3].editHtml;
-	var dom = $.parseHTML(html);
-	var text = dom
-	var des = $('textarea[name=description]', dom).html();
-	var check = ( /{panel:title=.*}[\s\S]([\s\S]*)[\s\S]{panel}/i.exec(des) )[1];
-	var list = check.replace('\r','').split('\n');
-
-
-
-	if (/\(\/\)/.test(list[index])) {
-		list[index] = list[index].replace(/\(\/\)/,"(x)");
-		$(".panel img.emoticon").eq(index).attr("src", "/images/icons/emoticons/error.png");
-		//$(element)
-	}
-	else if (/\(x\)/.test(list[index])) {
-		list[index] = list[index].replace(/\(x\)/,"(/)");
-		console.log($(".panel img.emoticon").eq(index).attr("src"));
-		$(".panel img.emoticon").eq(index).attr("src", "/images/icons/emoticons/check.png");
-		console.log($(".panel img.emoticon").eq(index).attr("src"));
-	}
-
-	var list2 = list.join("\r\n");
-
-	var des2 = des.replace(/({panel:title=.*}[\s\S])([\s\S]*)([\s\S]{panel})/i, "$1" + list2 + "$3");
-
+(function() {
+	var issueId = $("input[name=id]").val();
 	var token = $("input[name=atl_token]").val();
 
-	$.ajax({
-	  url: "https://sheknows.jira.com/secure/AjaxIssueAction.jspa?decorator=none",
-	  data: {description:des2,issueId:31078,atl_token:token,singleFieldEdit:true,fieldsToForcePresent:'description'},
-	  type: 'POST',
-	  //success: success,
-	  dataType: 'json'
+	$("#description-val .panel").remove().clone().insertAfter("#description-val");
+
+	$(".panel img.emoticon").hover(function(e) {
+		$(this).attr('height', 18);
+		$(this).attr('width', 18);
+		$(this).css('cursor', 'pointer');
+	}, function() {
+		$(this).attr('height', 16);
+		$(this).attr('width', 16);
+		$(this).css('cursor', 'default');
 	});
 
-	console.log(list);
-	console.log(des2);
-}
+	$(".panel img.emoticon").click(function(e) {
+		index = $(".panel img.emoticon").index(this);
 
-$("#description-val .panel").remove().clone().insertAfter("#description-val");
+		var des = getDescription();
+		var list = parseDes(des);
 
+		if (/\(\/\)/.test(list[index])) {
+			list[index] = list[index].replace(/\(\/\)/,"(x)");
+			$(".panel img.emoticon").eq(index).attr("src", "/images/icons/emoticons/error.png");
+		}
+		else if (/\(x\)/.test(list[index])) {
+			list[index] = list[index].replace(/\(x\)/,"(/)");
+			$(".panel img.emoticon").eq(index).attr("src", "/images/icons/emoticons/check.png");
+		}
 
-$(".panel img.emoticon").click(function(e) {
-	index = $(".panel img.emoticon").index(this);
-	console.log(index);
-
-	$.ajax({
-	  url: "https://sheknows.jira.com/secure/AjaxIssueEditAction!default.jspa",
-	  data: {decorator:'none',issueId:31078},
-	  type: 'GET',
-	  success: success,
-	  dataType: 'json'
+		var newDes = genDes(des, list);
+		setDescription(newDes);
 	});
-});
+
+
+	var parseDes = function(des) {
+		var check = ( /{panel:title=.*}[\s\S]([\s\S]*)[\s\S]{panel}/i.exec(des) )[1];
+		var list = check.replace('\r','').split('\n');
+
+		return list;
+	}
+
+	var genDes = function(des, list) {
+		var list2 = list.join("\r\n");
+		var des2 = des.replace(/({panel:title=.*}[\s\S])([\s\S]*)([\s\S]{panel})/i, "$1" + list2 + "$3");
+
+		return des2;
+	}
+
+	var getDescription = function() {
+		var description;
+
+		$.ajax({
+			url: "https://sheknows.jira.com/secure/AjaxIssueEditAction!default.jspa",
+		  	data: {decorator:'none',issueId:issueId},
+		  	type: 'GET',
+		  	async: false,
+		  	success: function(data) {
+		  		var html = data.fields[3].editHtml;
+				var dom = $.parseHTML(html);
+				var text = dom
+				description = $('textarea[name=description]', dom).html();
+		  	},
+		  	dataType: 'json'
+		});
+
+		return description;
+	}
+
+	var setDescription = function(des) {
+		$.ajax({
+		  	url: "https://sheknows.jira.com/secure/AjaxIssueAction.jspa?decorator=none",
+		  	data: {description:des,issueId:issueId,atl_token:token,singleFieldEdit:true,fieldsToForcePresent:'description'},
+		  	type: 'POST',
+		  	dataType: 'json'
+		});
+	}
+})();
